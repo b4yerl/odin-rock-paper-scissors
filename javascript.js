0a49f2c6068dfb2cfcde181e22893e454fdc3979
b4yerl <gcbayerlc@gmail.com>
2022-08-18 18:31:25 -0300

    Implementada a validação da escolha do usuário

--- javascript.js
+++ javascript.js
@@ -7,6 +7,17 @@ function playRound(playerSelection) {
     let player = playerSelection.toLowerCase();
     let computer = getComputerChoice();
     const computerObject = ['Rock', 'Paper', 'Scissors'];
+    //validação do input
+    let teste = 0;
+    for (let i = 0; i < 3; i++) {
+        if (player == computerObject[i].toLowerCase())
+        {
+            i = 3;
+            teste++;
+        }
+    }
+    if (teste == 0) {return 'Please insert a valid value'};
+    //condições de vitória
     if ((computer == 0 && player == "scissors") || (computer == 1 && player == "rock") || (computer == 2 && player == "paper"))
     {
         return `You Lose! ${computerObject[computer]} beats ${player}`;
